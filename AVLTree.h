c9634066b52b5aaaaee06122259bca7d5439b050
roeiV789 <roei_vager@DESKTOP-IO9EV9D.>
2025-05-14 21:26:39 +0300

    added git ignore file and created AVL tree generic class file

---
+++ AVLTree.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template<class T>
+class AVLTree{
+     struct Node{
+           T data;
+           Node* left;
+           Node* right;
+     };
+    Node* root;
+    public:
+
+
+};
