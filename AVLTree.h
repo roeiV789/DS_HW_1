ea2bf8793b4b00cd7a786002038f03a664391360
George (Gosha) Ginzburg <ginzburggeorgy@gmail.com>
2025-05-16 21:02:02 +0300

    separate declaration and definition of AVL class

--- AVLTree.h
+++ AVLTree.h
@@ -7,74 +7,33 @@ class AVLTree {
         T data;
         K key;
         int height;
-        Node* father;
+        Node *father;
         Node *left;
         Node *right;
 
-        Node(const T &data, const K& key) : data(data), key(key), height(1), father(nullptr), left(nullptr), right(nullptr) {
-        }
+        Node(const T &data, const K &key);
     };
 
-    void updateHeight(Node *node) {
-        node->height = 1 + max(node->left->height, node->right->height);
-    }
+    void updateHeight(Node *node);
 
     Node *root;
 
 public:
-    AVLTree() : root(nullptr) {
-    }
+    AVLTree();
 
-    ~AVLTree() {
-        AVLTree *temp = root;
-        deleteTree(temp);
-        root = nullptr;
-    }
+    ~AVLTree();
 
-    void deleteTree(Node *head) {
-        if (head == nullptr) {
-            return;
-        }
-        deleteTree(head->left);
-        deleteTree(head->right);
-        delete head;
-    }
+    void deleteTree(Node *head);
 
-    Node* rotateRight(Node* b) {
-        Node* a = b->left;
-        b->left = a->right;
-        a->right = b;
-        updateHeight(a);
-        updateHeight(b);
+    Node *rotateRight(Node *b);
 
-        return a;
-    }
+    Node *rotateLR(Node *b);
 
-    Node* rotateLR(Node* b) {
-        b->left = rotateLeft(b->left);
-        return rotateRight(b);
-    }
-    int getBalance(Node* node) const {
-        return node->left->height - node->right->height;
-    }
-    int getRoot() const {
-        return root;
-    }
+    int getBalance(Node *node) const;
 
-    T* search(const K& key) const{
-        return searchAux(root,key);
-    }
-    T* searchAux(Node* cur, const K& key) const {
-        if(cur == nullptr) {
-            return nullptr;
-        }
-        if(cur->key == key) {
-            return cur->data;
-        }
-        if(key < cur->key) {
-            return searchAux(cur->left, key);
-        }
-        return searchAux(cur->right, key);
-    }
+    int getRoot() const;
 
+    T *search(const K &key) const;
+
+    T *searchAux(Node *cur, const K &key) const;
 };
